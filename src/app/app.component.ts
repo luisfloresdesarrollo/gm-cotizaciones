af8bc5fa0ca6e3f923321082600614c60dd85268
luisflores25 <luis.francisco.flores.cruz@uabc.edu.mx>
2026-05-20 14:06:51 -0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { TableComponent } from './components/table/table.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CardComponent, TableComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
