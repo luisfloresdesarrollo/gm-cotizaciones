8137bfb1ca2e562a593e419e4934e73a5dd2409e
luisflores25 <luis.francisco.flores.cruz@uabc.edu.mx>
2026-05-20 16:05:37 -0700

    (feat) table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,25 @@ import { TableComponent } from './components/table/table.component';
 })
 export class AppComponent {
   title = 'gm-cotizaciones';
+
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'Luis',
+      correo: 'luis@mail.com',
+      edad: 28
+    },
+    {
+      id: 2,
+      nombre: 'Ana',
+      correo: 'ana@mail.com',
+      edad: 31
+    },
+    {
+      id: 3,
+      nombre: 'Carlos',
+      correo: 'carlos@mail.com',
+      edad: 24
+    }
+  ];
 }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
+  @Input() data: any[] = [];
 
+  columns: string[] = [];
+
+  ngOnChanges(): void {
+    if (this.data && this.data.length > 0) {
+      this.columns = Object.keys(this.data[0]);
+    }
+  }
 }
