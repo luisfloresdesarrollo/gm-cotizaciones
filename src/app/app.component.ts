5335287b36796d4eed217d9b50fa496ba7bd002d
luisflores25 <luis.francisco.flores.cruz@uabc.edu.mx>
2026-05-21 10:14:19 -0700

    (feat) header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CardComponent } from './components/card/card.component';
 import { TableComponent } from './components/table/table.component';
+import { Header } from './components/header/header';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CardComponent, TableComponent],
+    imports: [RouterOutlet, CardComponent, TableComponent, Header],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent{
   title = 'gm-cotizaciones';
-
+  ImgGM = 'https://gmterpv8puntocdn.azureedge.net/gmterpv8/GMTERPV8_WEB/Imagenes/LogoGM%20LOGIN.svg';
   usuarios = [
     {
       id: 1,
